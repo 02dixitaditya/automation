ef2e46f8fc269326cba54df6fabe720cae501ff6
02dixitaditya <37414494+02dixitaditya@users.noreply.github.com>
2023-02-20 15:24:34 +0530

    Update py_filter_csv.py

--- py_filter_csv.py
+++ py_filter_csv.py
@@ -1,1 +1,13 @@
-print('hello world')
+#pip install xlrd==1.2.0
+#pip install openpyxl
+import pandas as pd
+#fname=str(input("Enter file name"))
+import sys
+fname=sys.argv[1]
+df=pd.read_csv(fname)
+df_CVE=df[df['Vulnerability Name(s)'].str.contains('CVE',na=False)]
+df_hc=df_CVE[(df_CVE['Severity - cvss3']=='HIGH') | (df_CVE['Severity - cvss3']=='CRITICAL')]
+df_hc.loc[:,'Vulnerability Name(s)']=df_hc.loc[:,'Vulnerability Name(s)'].str.split(' ').str[0]
+df_to_save=df_hc[['Project Version','Component Name','Component Version Name','Package Path','Vulnerability Name(s)','Severity - cvss3','Upgrade Guidance - Short Term','Upgrade Guidance - Long Term']]
+df_to_save.to_csv(fname[:len(fname)-4]+'_filtered.csv',index=False)
+
